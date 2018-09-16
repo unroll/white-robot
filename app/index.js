93013d701b654a868437f07ca85357bb65e46802
Lin Jinxing <172601673@qq.com>
2018-09-16 11:46:44 +0800

    增加静态文件及css的处理

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i from './assets/img.png';
+import s from './assets/ss.svg';
+import './index.css';
 
-const a = (<a href='https://baidu.com'>baidu</a>);
+
+const a = (<img src={s}/>);
 ReactDOM.render(a, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const development = false;
 module.exports = {
-    mode: "development",
+    mode: development ? 'development' : 'production',
     entry: {
         app: path.join(__dirname, 'app', 'index.js')
     },
@@ -16,17 +19,48 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /.js(x?)$/,
-            loader: ['babel-loader', 'eslint-loader'],
-            exclude: [
-                path.join(__dirname, 'node_modules')
-            ]
-        }]
+                test: /\.js(x?)$/,
+                use: ['babel-loader', 'eslint-loader'],
+                exclude: path.join(__dirname, 'node_modules')
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    development ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [require('autoprefixer')]
+                        }
+                    }
+                ],
+                exclude: [
+                    path.join(__dirname, 'node_modules')
+                ]
+            },
+            {
+                test: /\.jpe?g|png|svg|webp$/,
+                use: { loader: 'url-loader', options: { limit: 8192 } }
+            },
+            {
+                test: /\.jpe?g|png|webp|gif$/,
+                loader: 'file-loader',
+                exclude: path.join(__dirname, 'node_modules'),
+                options: {
+                    name: 'assets/[name].[hash:8].[ext]'
+                }
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'app', 'index.html')
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css'
+        }),
+        new CleanWebpackPlugin(path.join(__dirname, 'dist'))
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
@@ -36,5 +70,6 @@ module.exports = {
         overlay: {
             errors: true
         },
+        open: true
     }
-}
+};
